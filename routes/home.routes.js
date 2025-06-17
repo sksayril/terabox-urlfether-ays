2f71fd0c78725ad5386ece9d599874c65b4f2c51
sksayril <sksayril123@gmail.com>
2025-06-18 00:39:57 +0530

    chnages

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const Home = require('../models/home.model');
 const { upload, handleS3UploadError } = require('../utilities/s3Config');
 const { requireAdmin } = require('../utilities/auth');
-
+const axios = require('axios');
+const FormData = require('form-data');
 // Upload home thumbnail (Admin only)
 router.post('/thumbnail', requireAdmin, upload.single('thumbnail'), handleS3UploadError, async (req, res) => {
     try {
@@ -123,15 +124,6 @@ router.get('/', async (req, res) => {
         });
     }
 });
-const staticResponse = {
-  "file_name": "Peaky Blinders S05E04 720p BluRay Dual Audio [Hindi 2.0 + English 5.1] AAC ×264 ESub - Moweblinks.mkv",
-  "download_link": "https://dm-d.terabox.app/file/aeebbee541df15073380800b581174fc?fid=81365278609555-250528-450839605716529&dstime=1749043939&rt=sh&sign=FDtAER-DCb740ccc5511e5e8fedcff06b081203-DVGVYLgN27xvvSkIa6nw%2F%2BXmG5w%3D&expires=8h&chkv=0&chkbd=0&chkpc=&dp-logid=133744601787304275&dp-callid=0&r=712101289&sh=1&region=dm",
-  "thumbnail": "https://dm-data.terabox.app/thumbnail/aeebbee541df15073380800b581174fc?fid=81365278609555-250528-450839605716529&time=1749042000&rt=sh&sign=FDTAER-DCb740ccc5511e5e8fedcff06b081203-guo9uEF1QIWw8LENGbgtmDIyHgE%3D&expires=8h&chkv=0&chkbd=0&chkpc=&dp-logid=133744601787304275&dp-callid=0&size=c850_u580&quality=100&vuk=-&ft=video",
-  "file_size": "592.74 MB",
-  "size_bytes": 621531936,
-  "proxy_url": "https://terabox.0xproxy.workers.dev/proxy?url=https%3A%2F%2Fdm-d.terabox.app%2Ffile%2Faeebbee541df15073380800b581174fc%3Ffid%3D81365278609555-250528-450839605716529%26dstime%3D1749043939%26rt%3Dsh%26sign%3DFDtAER-DCb740ccc5511e5e8fedcff06b081203-DVGVYLgN27xvvSkIa6nw%252F%252BXmG5w%253D%26expires%3D8h%26chkv%3D0%26chkbd%3D0%26chkpc%3D%26dp-logid%3D133744601787304275%26dp-callid%3D0%26r%3D712101289%26sh%3D1%26region%3Ddm&file_name=Peaky%20Blinders%20S05E04%20720p%20BluRay%20Dual%20Audio%20%5BHindi%202.0%20%2B%20English%205.1%5D%20AAC%20%C3%97264%20ESub%20-%20Moweblinks.mkv"
-};
-
 router.post('/teralink', async (req, res) => {
     const { url } = req.body;
 
@@ -142,17 +134,38 @@ router.post('/teralink', async (req, res) => {
         });
     }
 
-    // You can log or validate the url here if needed
-    console.log('Received URL:', url);
+    try {
+        // Prepare FormData as TeraBox API expects
+        const form = new FormData();
+        form.append('url', url);
+        form.append('action', 'terabox_api_request');
 
-    // Send back static response
-    res.status(200).json({
-        success: true,
-        data: staticResponse
-    });
+        const response = await axios.post('https://terabxdownloader.com/wp-admin/admin-ajax.php', form, {
+            headers: form.getHeaders()
+        });
+
+        // Assume response.data contains:
+        // title, size, thumbnail, download_link
+        const { title, size, thumbnail, download_link } = response.data;
+
+        res.json({
+            success: true,
+            data: {
+                title,
+                size,
+                thumbnail,
+                download_link
+            }
+        });
+
+    } catch (error) {
+        console.error('TeraBox parsing error:', error.message);
+        res.status(500).json({
+            success: false,
+            message: 'Failed to fetch TeraBox link details'
+        });
+    }
 });
-
-
 
 // Get home content for admin
 router.get('/admin', requireAdmin, async (req, res) => {
